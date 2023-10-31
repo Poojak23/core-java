f5813cee04c6752d5fdff021ed6cba2531fa86e2
Pooja292000 <gowdapoojak292000@gmail.com>
2023-10-31 15:37:05 +0530

    constructer

---
+++ ShowroomStarter.java
@@ -0,0 +1,113 @@
+class ShowroomStarter
+{
+	public static void main(String[] args)
+	{
+		System.out.println("starting main in ShowroomStarter");
+		
+		Showroom showroom=new Showroom();
+		System.out.println(" showroom name is:"+showroom.name);
+		System.out.println(" showroom pincode is:"+showroom.pincode);
+		System.out.println(" showroom number is:"+showroom.phno);
+		System.out.println(" showroom location is:"+showroom.location);
+		System.out.println(" noOfWorkers in showroom is:"+showroom.noOfWorkers);
+		System.out.println(" showroom owner name is:"+showroom.ownerName);
+		System.out.println("showroom shape is:"+showroom.shape);
+		System.out.println(" showroom type is:"+showroom.type);
+		
+		
+		
+		Showroom showroom1=new Showroom("Cobike Cycle");
+		System.out.println(" showroom name is:"+showroom1.name);
+		System.out.println(" showroom pincode is:"+showroom1.pincode);
+		System.out.println(" showroom number is:"+showroom1.phno);
+		System.out.println(" showroom location is:"+showroom1.location);
+		System.out.println(" noOfWorkers in showroom is:"+showroom1.noOfWorkers);
+		System.out.println(" showroom owner name is:"+showroom1.ownerName);
+		System.out.println("showroom shape is:"+showroom1.shape);
+		System.out.println(" showroom type is:"+showroom1.type);
+		
+		Showroom showroom2=new Showroom("Well done Wheels",572134);
+		System.out.println(" showroom name is:"+showroom2.name);
+		System.out.println(" showroom pincode is:"+showroom2.pincode);
+		System.out.println(" showroom number is:"+showroom2.phno);
+		System.out.println(" showroom location is:"+showroom2.location);
+		System.out.println(" noOfWorkers in showroom is:"+showroom2.noOfWorkers);
+		System.out.println(" showroom owner name is:"+showroom2.ownerName);
+		System.out.println("showroom shape is:"+showroom2.shape);
+		System.out.println(" showroom type is:"+showroom2.type);
+		
+		Showroom showroom3=new Showroom("Well done Wheels",570034,93539307l);
+		System.out.println(" showroom name is:"+showroom3.name);
+		System.out.println(" showroom pincode is:"+showroom3.pincode);
+		System.out.println(" showroom number is:"+showroom3.phno);
+		System.out.println(" showroom location is:"+showroom3.location);
+		System.out.println(" noOfWorkers in showroom is:"+showroom3.noOfWorkers);
+		System.out.println(" showroom owner name is:"+showroom3.ownerName);
+		System.out.println("showroom shape is:"+showroom3.shape);
+		System.out.println(" showroom type is:"+showroom3.type);
+		
+		Showroom showroom4=new Showroom(5721900,9141162489l,"Nick jhonson");
+		System.out.println(" showroom name is:"+showroom4.name);
+		System.out.println(" showroom pincode is:"+showroom4.pincode);
+		System.out.println(" showroom number is:"+showroom4.phno);
+		System.out.println(" showroom location is:"+showroom4.location);
+		System.out.println(" noOfWorkers in showroom is:"+showroom4.noOfWorkers);
+		System.out.println(" showroom owner name is:"+showroom4.ownerName);
+		System.out.println("showroom shape is:"+showroom4.shape);
+		System.out.println(" showroom type is:"+showroom4.type);
+		
+		Showroom showroom5=new Showroom(5721,"Hebbal");
+		System.out.println(" showroom name is:"+showroom5.name);
+		System.out.println(" showroom pincode is:"+showroom5.pincode);
+		System.out.println(" showroom number is:"+showroom5.phno);
+		System.out.println(" showroom location is:"+showroom5.location);
+		System.out.println(" noOfWorkers in showroom is:"+showroom5.noOfWorkers);
+		System.out.println(" showroom owner name is:"+showroom5.ownerName);
+		System.out.println("showroom shape is:"+showroom5.shape);
+		System.out.println(" showroom type is:"+showroom5.type);
+		
+		Showroom showroom6=new Showroom(500,"square","BTM layout");
+		System.out.println(" showroom name is:"+showroom6.name);
+		System.out.println(" showroom pincode is:"+showroom6.pincode);
+		System.out.println(" showroom number is:"+showroom6.phno);
+		System.out.println(" showroom location is:"+showroom6.location);
+		System.out.println(" noOfWorkers in showroom is:"+showroom6.noOfWorkers);
+		System.out.println(" showroom owner name is:"+showroom6.ownerName);
+		System.out.println("showroom shape is:"+showroom6.shape);
+		System.out.println(" showroom type is:"+showroom6.type);
+	
+	    Showroom showroom7=new Showroom(500,"rectangle","Car showroom",984599125l);
+		System.out.println(" showroom name is:"+showroom7.name);
+		System.out.println(" showroom pincode is:"+showroom7.pincode);
+		System.out.println(" showroom number is:"+showroom7.phno);
+		System.out.println(" showroom location is:"+showroom7.location);
+		System.out.println(" noOfWorkers in showroom is:"+showroom7.noOfWorkers);
+		System.out.println(" showroom owner name is:"+showroom7.ownerName);
+		System.out.println("showroom shape is:"+showroom7.shape);
+		System.out.println(" showroom type is:"+showroom7.type);
+	
+	    Showroom showroom8=new Showroom(500,"rectangle","Car showroom");
+		System.out.println(" showroom name is:"+showroom8.name);
+		System.out.println(" showroom pincode is:"+showroom8.pincode);
+		System.out.println(" showroom number is:"+showroom8.phno);
+		System.out.println(" showroom location is:"+showroom8.location);
+		System.out.println(" noOfWorkers in showroom is:"+showroom8.noOfWorkers);
+		System.out.println(" showroom owner name is:"+showroom8.ownerName);
+		System.out.println("showroom shape is:"+showroom8.shape);
+		System.out.println(" showroom type is:"+showroom8.type);
+		
+		 Showroom showroom9=new Showroom("Tata Motors",920091,9845621119l,"rathan tata",500,"round","Car showroom");
+		System.out.println(" showroom name is:"+showroom9.name);
+		System.out.println(" showroom pincode is:"+showroom9.pincode);
+		System.out.println(" showroom number is:"+showroom9.phno);
+		System.out.println(" showroom location is:"+showroom8.location);
+		System.out.println(" noOfWorkers in showroom is:"+showroom9.noOfWorkers);
+		System.out.println(" showroom owner name is:"+showroom9.ownerName);
+		System.out.println("showroom shape is:"+showroom9.shape);
+		System.out.println(" showroom type is:"+showroom9.type);
+	
+	
+	
+
+	}
+}
