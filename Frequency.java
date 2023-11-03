2c1944769e4c3f06e5e73dccd1043f58aaca4ff3
Pooja292000 <gowdapoojak292000@gmail.com>
2023-11-03 22:28:26 +0530

    logic

---
+++ Frequency.java
@@ -0,0 +1,18 @@
+class Frequency
+{
+	
+	static int frequency(int[] a,int x)
+	{
+		System.out.println("running frequency in Frequency1");
+		int count=0;
+		for( int i=0; i<a.length;i++)
+			if(a[i]==x)
+			    count++;
+			return count;
+		
+	}
+	
+
+}
+
+   
